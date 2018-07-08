5c6097cac1db29c7a4c51eecab13f4410bf86015
ankala wu <729923753@qq.com>
2018-07-08 15:23:11 +0800

    Update views.py

--- django-study/basic_study/basic_study/views.py
+++ django-study/basic_study/basic_study/views.py
@@ -2,6 +2,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
+def index(request):
+    return render(request,'index.html')
+
 def hello(request):
 #    return HttpResponse("Hello world ! ")
     context          = {}
